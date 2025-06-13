c08b92321e29aadbba3e17eaef24cd41882f03a2
Whitefox23321 <afdhalhadisolahudin@gmail.com>
2025-06-13 13:18:02 +0700

    Add : Favicon.ico

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/dufan-min.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
